35bb505bc987917b6a2e6154aa494ce1993415ec
Matthew Latham <50916823+matlat8@users.noreply.github.com>
2024-12-16 19:05:47 -0500

    feat: Add ClickHouse database connection to health endpoint
    This commit adds the ClickHouse database connection to the health endpoint in the `app.py` file. Now, when the health endpoint is called, it will also check the connection to the ClickHouse database and include the database status in the response.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from src.drivers.router import drivers as drivers_router
+from src.connections.db import ClickhouseConn
 from version import __version__
 
 app = FastAPI()
@@ -8,5 +9,5 @@ app = FastAPI()
 app.include_router(drivers_router)
 
 @app.get("/health")
-async def get_health():
-    return {"status": "ok", "version": __version__}
+async def get_health(ch: ClickhouseConn):
+    return {"status": "ok", "version": __version__, "db": await ch.check_connection_to_click_house()}
