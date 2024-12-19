5a12f28fca93fa2fbdd1bc7aae45efc296406e69
Matthew Latham <50916823+matlat8@users.noreply.github.com>
2024-12-18 20:22:14 -0500

    chore: Update FastAPI app initialization
    The code changes update the initialization of the FastAPI app in the `app.py` file. The `app_name` and `version` parameters are now passed to the `FastAPI` constructor using values from the `settings` module. This ensures that the app name and version are set correctly.

--- src/app.py
+++ src/app.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 from src.drivers.router import drivers as drivers_router
 from src.connections.db import ClickhouseConn
-from version import __version__
 
-app = FastAPI()
+from version import __version__
+from config import settings
+
+app = FastAPI(app_name=settings.app_name, version=__version__)
 
 app.include_router(drivers_router)
 
