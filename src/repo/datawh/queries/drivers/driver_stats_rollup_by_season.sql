acd7cf02a856eb824a953e39331acf93120e9130
Matthew Latham <50916823+matlat8@users.noreply.github.com>
2025-01-08 22:00:51 -0500

    refactor: Update driver_stats_rollup_by_season.sql query to calculate average finish and start positions
    The code changes modify the `driver_stats_rollup_by_season.sql` query to calculate the average finish and start positions for a specific customer ID. The query now uses the `round` function to round the average finish and start positions to two decimal places. This change is made to provide more accurate and meaningful driver statistics by season.

--- src/repo/datawh/queries/drivers/driver_stats_rollup_by_season.sql
+++ src/repo/datawh/queries/drivers/driver_stats_rollup_by_season.sql
@@ -6,9 +6,9 @@ select
     sum(if(sr.simsession_type = 6 AND sr.finish_position_in_class = 0, 1, 0)) AS total_wins,
     sum(if(sr.simsession_type = 6 AND sr.finish_position_in_class <= 2, 1, 0)) AS podiums,
     sum(if(sr.simsession_type = 6 AND sr.finish_position_in_class <= 4, 1, 0)) AS top_5,
-    avg(if(sr.simsession_type = 6, sr.laps_complete, null)) as irating_events_avg_laps_completed,
-    round(avg(sr.finish_position_in_class), 2) + 1 as avg_finish_position_in_class,
-    round(avg(sr.starting_position_in_class), 2) + 1 as avg_start_position_in_class
+    round(avg(if(sr.simsession_type = 6, sr.laps_complete, null)), 2) as irating_events_avg_laps_completed,
+    round(avg(sr.finish_position_in_class)+ 1, 2)  as avg_finish_position_in_class,
+    round(avg(sr.starting_position_in_class)+ 1, 2)  as avg_start_position_in_class
 from iracing.mv_session_results sr
 inner join iracing.v_series s
     on sr.subsession_id = s.subsession_id
