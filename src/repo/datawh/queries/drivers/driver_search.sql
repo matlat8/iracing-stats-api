b7acb4cfdba1fb5255af7ad5fd104f2a76569710
Matthew Latham <50916823+matlat8@users.noreply.github.com>
2025-02-14 20:43:18 -0500

    feat: Improve driver search query
    The code changes in `driver_search.sql` modify the driver search query to select all columns instead of specific ones. Additionally, the query is now ordered by `cust_id` in descending order. This improvement enhances the driver search functionality by retrieving more comprehensive information and presenting the results in a more meaningful order.

--- src/repo/datawh/queries/drivers/driver_search.sql
+++ src/repo/datawh/queries/drivers/driver_search.sql
@@ -1,14 +1,11 @@
 SELECT 
-    cust_id, 
-    display_name,
-    club_name,
-    country_code
+    *
 FROM 
     iracing_prod_model.dim_drivers
 WHERE 
     display_name ILIKE %(search_term)s
 ORDER BY 
-    cust_id
+    cust_id desc
 LIMIT 
     %(limit)s
 OFFSET %(offset)s
